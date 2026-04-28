2a80f1e139ce39e8c663015810f173c6753bd89b
dvnguyen02 <duynguyen290502@gmail.com>
2026-04-28 20:59:15 +1200

    test: m2 integration tests

--- tests/BlobServer.Tests/UnitTest1.cs
+++ tests/BlobServer.Tests/UnitTest1.cs
@@ -42,4 +42,24 @@ public class BLobEndpointTests : IClassFixture<WebApplicationFactory<Program>>
         var response = await _client.GetAsync("/testcontainer/deleteme.txt");
         Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
     }
+
+    [Fact]
+    public async Task GetWithMatchingETag_Returns304()
+    {
+        // upload a blob
+        var content = new StringContent("test content", System.Text.Encoding.UTF8, "text/plain");
+        var put = await _client.PutAsync("/testcontainer/etag-test.txt", content);
+        put.EnsureSuccessStatusCode();
+        // grab etag from the response 
+        var etag = put.Headers.ETag?.ToString();
+
+        // get with that etag in if-none-match
+        var request = new HttpRequestMessage(HttpMethod.Get, "/testcontainer/etag-test.txt");
+        request.Headers.IfNoneMatch.Add(new System.Net.Http.Headers.EntityTagHeaderValue(etag!));
+        var get = await _client.SendAsync(request);
+
+        Assert.Equal(System.Net.HttpStatusCode.NotModified, get.StatusCode);
+
+
+    }
 }
